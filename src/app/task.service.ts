747a15b2f723a410901bafdeefcb8770a859336d
Clemens Horn <clemens.horn@mni.thm.de>
2021-11-03 17:37:07 +0100

    add task service
    Signed-off-by: Clemens Horn <clemens.horn@mni.thm.de>

---
+++ src/app/task.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { Task } from './model/task';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+  constructor() { }
+
+  _taskList: Task[] = [
+    new Task('Aufgabe'),
+    new Task('Noch eine Aufgabe'),
+  ]
+
+  get taskList(): Task[] {
+    return this._taskList;
+  }
+
+  addTasK(task: Task) {
+    if (task.title.trim() == "") {
+      return;
+    }
+    this.taskList.push(new Task(task.title));
+  }
+}
