03667e289a18ed9a981ab9613a8c257532bb9479
Clemens Horn <clemens.horn@mni.thm.de>
2021-11-03 17:58:18 +0100

    add task-list component
    Signed-off-by: Clemens Horn <clemens.horn@mni.thm.de>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { TaskComponent } from './task/task.component';
+import { TaskListComponent } from './task-list/task-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TaskComponent
+    TaskComponent,
+    TaskListComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/task-list/task-list.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { TaskService } from '../task.service';
+
+@Component({
+  selector: 'app-task-list',
+  templateUrl: './task-list.component.html',
+  styleUrls: ['./task-list.component.scss']
+})
+export class TaskListComponent implements OnInit {
+
+  constructor(public taskService: TaskService) {
+    console.log(taskService.taskList);
+  }
+
+  ngOnInit(): void {
+  }
+
+}
