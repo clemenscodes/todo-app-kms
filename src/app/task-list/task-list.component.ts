08f9d5d5456607700c9a1d53244e8672b823d9f6
Clemens Horn <clemens.horn@mni.thm.de>
2021-11-03 18:06:02 +0100

    render tasks
    Signed-off-by: Clemens Horn <clemens.horn@mni.thm.de>

--- src/app/task-list/task-list.component.ts
+++ src/app/task-list/task-list.component.ts
@@ -1,16 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { TaskService } from '../task.service';
+import { Task } from '../model/task'
 
 @Component({
   selector: 'app-task-list',
   templateUrl: './task-list.component.html',
-  styleUrls: ['./task-list.component.scss']
+  styleUrls: ['./task-list.component.scss'],
 })
 export class TaskListComponent implements OnInit {
+  constructor(public taskService: TaskService) {}
 
-  constructor(public taskService: TaskService) { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  get taskList(): Task[] {
+    return this.taskService.taskList;
   }
-
 }
