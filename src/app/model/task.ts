d2ae1f07812495586eb298ee18a6b08bec13eb2f
Clemens Horn <clemens.horn@mni.thm.de>
2021-11-03 17:30:10 +0100

    add task class model
    Signed-off-by: Clemens Horn <clemens.horn@mni.thm.de>

---
+++ src/app/model/task.ts
@@ -0,0 +1,11 @@
+export class Task {
+  private static id = 1;
+  constructor(title: string, done?: boolean) {
+    this.title = title;
+    this.done = false;
+    this.id = Task.id++;
+  }
+  id: number;
+  title: string;
+  done: boolean;
+}
