4bfb68d1c2fa321ae4d5f1d36d4866576209713f
Clemens Horn <clemens.horn@mni.thm.de>
2021-11-08 17:11:35 +0100

    add edit button
    Signed-off-by: Clemens Horn <clemens.horn@mni.thm.de>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { AppComponent } from './app.component';
 import { TaskListComponent } from './task-list/task-list.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { TaskComponent } from './task/task.component';
-import { AddTaskComponent } from './add-task/add-task.component';
 import {FormsModule} from "@angular/forms";
 import { TaskEditComponent } from './task-edit-modal/task-edit-modal.component';
 
