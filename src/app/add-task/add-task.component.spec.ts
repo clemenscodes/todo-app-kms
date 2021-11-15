ccaf929fc35db92e10c3b51e228411b00bc0be1b
Tim Eggers <tim.luca.eggers@mni.thm.de>
2021-11-15 12:23:01 +0100

    fix test

--- src/app/add-task/add-task.component.spec.ts
+++ src/app/add-task/add-task.component.spec.ts
@@ -8,7 +8,6 @@ describe('AddTaskComponent', () => {
   let component: AddTaskComponent;
   let fixture: ComponentFixture<AddTaskComponent>;
   let inputText: HTMLInputElement;
-  let submitButton: HTMLButtonElement;
   let taskService: TaskService;
 
   beforeEach(async () => {
@@ -27,46 +26,53 @@ describe('AddTaskComponent', () => {
     fixture.detectChanges();
     inputText = fixture.nativeElement.querySelector('input');
     taskService = TestBed.inject(TaskService);
+    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should have label "title of todo"', () => {
-    const label: HTMLElement = fixture.nativeElement.querySelector('label');
+  it('should have title "create new todo" ', () => {
+    let title: HTMLElement = fixture.nativeElement.querySelector('h5');
+    expect(title.innerText).toMatch("Create New Todo");
+  });
+
+  it('should have label "title of todo" ', () => {
+    let label: HTMLElement = fixture.nativeElement.querySelector('label');
     expect(label.innerText).toMatch("Title of ToDo");
   });
 
-  it('should have no entry in form text field', () => {
+  it('should have no entry in form text field "title" ', () => {
     expect(inputText.value).toEqual('');
   })
 
-  it('should add task with add function', () => {
+  it('should have button "submit"', () => {
+    let button: HTMLButtonElement = fixture.nativeElement.querySelector('button');
+    expect(button.innerText).toMatch("Submit");
+  });
+
+  it('should add task with addTask()', () => {
     expect(taskService.taskList).toHaveSize(2); // 2 dummy entries
     inputText.value = "test-task";
     component.addTask();
     expect(taskService.taskList).toHaveSize(3);
   })
 
-  it('should not add empty task with add function', () => {
-    expect(taskService.taskList).toHaveSize(2); // 2 dummy entries
+  it('should not add empty task with addTask()', () => {
+    expect(taskService.taskList).toHaveSize(2);
     inputText.value = "";
     component.addTask();
     expect(taskService.taskList).toHaveSize(2);
   })
 
-  it('should add task with submit button', () => {
-    expect(taskService.taskList).toHaveSize(2); // 2 dummy entries
+  it('should call submit button', async () => {
+    inputText.value = "test-task";
+    spyOn(component, 'addTask');
+    let btn = fixture.debugElement.query(By.css('button'));
+    btn.triggerEventHandler('click', null);  // else full page reload errors with btn.click()
     fixture.whenStable().then(() => {
-      inputText.value = "test-task";
-      spyOn(component, 'addTask');
-      // submitButton = fixture.debugElement.query(By.css('button')).nativeElement;
-      submitButton = fixture.nativeElement.querySelector('button');
-      //submitButton.click(); // not working
       expect(component.addTask).toHaveBeenCalled();
-      expect(taskService.taskList).toHaveSize(3);
     })
   })
-
 });
